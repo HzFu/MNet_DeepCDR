49c6733f6ed167626b212b9ee80b51a8782f0412
HzFu <huazhufu@gmail.com>
2018-02-26 15:03:32 +0800

    Add CDR function

---
+++ Demo_Calculate_CDR.m
@@ -0,0 +1,15 @@
+%
+clear; close all;
+
+img_folder = './test_img/';
+img_result = './result/';
+img_list = dir([img_folder '*.jpg']);
+
+img_num = size(img_list, 1);
+
+CDR_list = zeros([img_num, 1]);
+for idx = 1:img_num
+    img_name = img_list(idx).name;
+    load([img_result img_name(1:end-4) '.mat']);
+    CDR_list(idx) = fun_CalCDR( cat(3, Disc_map, Cup_map) );
+end
